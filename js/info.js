ed7716101632f26bdf50f4d21d33316b262ae571
SC1314520 <blc0000421@gmail.com>
2022-11-11 01:08:13 +0800

    s

--- js/info.js
+++ js/info.js
@@ -1,6 +1,8 @@
 var info =document.getElementById("info-text");
-console.log(info);
-    //使用這個表單
+var ru = document.querySelector(".ru");
+var sl=0;
+
+    // 公告欄
     var apiUri='https://script.google.com/macros/s/AKfycbw8qnHJhv5NUskphBWq3ONcAysku_DvxwFgSZPKbLzdxf2dlrfSK9qwYZRPVjwwWkmq/exec'
     sendData();
 
@@ -14,10 +16,23 @@ console.log(info);
           fetch(this.apiUri, config)
             .then(response => response.text())
             .then(result => {
-              console.log(result);
               var s =JSON.parse(result)["info"][0][0];
-              console.log(s);
               info.innerText=s;
+              sl=s.length;
+              var i=0;
+              var size = parseInt(window.getComputedStyle(info,null).getPropertyValue("font").split("/")[0]);
+              var width = parseInt(window.getComputedStyle(ru,null).getPropertyValue("width"));
+              var speed = size%2 == 0 ? 2:1;
+              var time = speed==2 ? 30:60;
+              setInterval(function(){
+                i-=speed;
+                info.style.left=i+"px";
+                if(i==(-sl*size)){
+                  i=width+16;
+                }
+              }, time);
             })
             .catch(error => console.log('error', error));
     }
+    
+    
